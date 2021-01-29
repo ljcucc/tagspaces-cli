12fd974f2e3911017b15f8e0fef0f2a130960f28
Jason Lin <linjason920523@gmail.com>
2021-01-29 09:50:03 +0800

    add new test things

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import terminal
 
 def run():
-    print(terminal.rgb_to_xterm(255,255,255))
+    # print(terminal.rgb_to_xterm(255,255,255))
+    terminal.setTextColor(0,122,255)
+    print("hi")
+
+    return True
