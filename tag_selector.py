d3a400638d6e84ec4e9f54d394097104ffb5517b
Jason Lin <linjason920523@gmail.com>
2021-01-29 14:50:11 +0800

    add cancel comfirm

--- tag_selector.py
+++ tag_selector.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from os import path
 import json
 import terminal
+from time import sleep
 
 selectedIndex = 0
 tags = {}
@@ -10,7 +11,13 @@ selectedTags = {}
 def displayTag():
     global selectedIndex
 
-    print("Add or remove tags with list below:\n(switch using SPACE key, Done using ENTER key, D key for add new tag) \n")
+    terminal.clearScreen()
+
+    terminal.setTextColor(255,255,255)
+    print("Add or remove tags with list below:")
+    terminal.setTextColor(100,100,100)
+    print("\n(switch using SPACE key, Done using ENTER key, \n\tD key for add new tag, \n\tQ for quit without save) \n")
+    terminal.setTextColor(255,255,255)
 
     for (index, title) in enumerate(tags):
         print(" "*4+"(" + ("\u001b[7m" if selectedIndex == index else "\u001b[0m") + ("*" if title in selectedTags else " ") + "\u001b[0m)", end="")
@@ -18,7 +25,7 @@ def displayTag():
         terminal.printTag(tags[title])
         print()
     print()
-    print("\033[%dA"% (5+ len(tags.keys())))
+    # print("\033[%dA"% (5+ len(tags.keys())))
 
 def readTags():
     global tags
@@ -46,8 +53,18 @@ def keyInput():
         selectedIndex += 1
     elif(mode == "k"):
         selectedIndex -= 1
+    elif(mode == "d" or mode == "D"):
+        addTag()
+    elif(mode.lower() == "q"):
+        terminal.clearScreen()
+        confirm = input("WARNING: Are you sure you want to quit without save? (Y or yes) ")
+        if(confirm.upper() == "Y" or confirm.upper() == "YES"):
+            terminal.clearScreen()
+            print("OK, Cancel it, won't do any change!")
+            sleep(1)
+            return
     elif(ord(mode) == 32):
-        print(list(tags.keys()))
+        # print(list(tags.keys()))
         title = list(tags.keys())[selectedIndex]
         if(not title in selectedTags):
             selectedTags.update({
@@ -55,7 +72,7 @@ def keyInput():
             })
         else:
             selectedTags.pop(title)
-        print("\033[1A",end="")
+        # print("\033[1A",end="")
 
     if(selectedIndex < 0):
         selectedIndex = 0
@@ -65,6 +82,25 @@ def keyInput():
 
     displayTag()
     keyInput()
+
+def addTag():
+    terminal.clearScreen()
+    print("# Add new tag:")
+    terminal.setTextColor(100,100,100)
+    print("WARNING: This aciton will not add any tag\n"+ (" "*9) +"into your global environment until you run `tags l -s`\n")
+    terminal.setTextColor(255,255,255)
+    name = input("name: ")
+    color = input("color (default #fff): ")
+
+    confirm = input("Sure you want this config? (Y for yes) ").upper()
+    if confirm == "YES" or confirm == "Y":
+        print("\nDone! adding...")
+    else:
+        print("\nTag won't add")
+    
+    sleep(1)
+        
+    terminal.clearScreen()
 
 def run():
 
