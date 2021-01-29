0249589b8ec97328a3ef627bf466d3155e244af4
Jason Lin <linjason920523@gmail.com>
2021-01-29 14:23:44 +0800

    add new UnitTest

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import terminal
 import file_selector as fs
+import tag_selector as ts
 
 def run():
     # print(terminal.rgb_to_xterm(255,255,255))
     # terminal.setTextColor(0,122,255)
     # print("hi")
 
-    fs.selectFile()
+    # fs.selectFile()
+
+    # print(ord(terminal.getchar()))
+
+    ts.run()
 
     return True
