9d9a3de2c5002ad9fd6ba35dfdcec13034c569ed
Jason Lin <linjason920523@gmail.com>
2021-01-29 11:46:59 +0800

    add new test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import terminal
+import file_selector as fs
 
 def run():
     # print(terminal.rgb_to_xterm(255,255,255))
-    terminal.setTextColor(0,122,255)
-    print("hi")
+    # terminal.setTextColor(0,122,255)
+    # print("hi")
+
+    fs.selectFile()
 
     return True
