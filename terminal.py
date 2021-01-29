0538e15f709ed9382a6fd8285774e3a3545a5d03
Jason Lin <linjason920523@gmail.com>
2021-01-29 10:53:59 +0800

    add color support

--- terminal.py
+++ terminal.py
@@ -16,3 +16,25 @@ def rgb_to_xterm(r, g, b):
             return 232 + c
 
     return 16 + 36*N[r] + 6*N[g] + N[b]
+
+def hex_to_rgb(hx, hsl=False):
+    import re
+    if re.compile(r'#[a-fA-F0-9]{3}(?:[a-fA-F0-9]{3})?$').match(hx):
+        div = 255.0 if hsl else 0
+        if len(hx) <= 4:
+            return tuple(int(hx[i]*2, 16) / div if div else
+                         int(hx[i]*2, 16) for i in (1, 2, 3))
+        return tuple(int(hx[i:i+2], 16) / div if div else
+                     int(hx[i:i+2], 16) for i in (1, 3, 5))
+    raise ValueError(f'"{hx}" is not a valid HEX code.')
+
+def printTag(tag):
+    colorCode = tag['color'][:-2]
+    rgbCode = hex_to_rgb(colorCode)
+
+    setTextColor(rgbCode[0], rgbCode[1],rgbCode[2])
+    print(tag['title'], end="")
+
+    setTextColor(255,255,255)
+    print(", ", end="")
+    
